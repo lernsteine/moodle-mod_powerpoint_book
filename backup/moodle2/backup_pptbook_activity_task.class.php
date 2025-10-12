71539a43e59285345481a7af4c143de92f5dc073
LERNSTEINE® <info@lernsteine.com>
2025-10-12 12:32:09 +0200

    Run phpcbf: auto-fix Moodle CS issues

--- backup/moodle2/backup_pptbook_activity_task.class.php
+++ backup/moodle2/backup_pptbook_activity_task.class.php
@@ -1,11 +1,10 @@
 <?php
+defined('MOODLE_INTERNAL') || die();
 // This file is part of Moodle - https://moodle.org/
-//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,7 +15,6 @@
 
 /**
  * Backup task for mod_pptbook.
- *
  * @package   mod_pptbook
  * @category  backup
  * @copyright 2025 Ralf Hagemeister
