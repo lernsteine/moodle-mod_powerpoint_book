d353bc47f90b9b44a4c297fbccae5cc4ae613dc1
LERNSTEINE® <info@lernsteine.com>
2025-10-12 12:23:43 +0200

    Run phpcbf: auto-fix Moodle CS issues

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,6 +1,6 @@
 <?php
-defined('MOODLE_INTERNAL') || die();
 // This file is part of Moodle - https://moodle.org/
+//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -13,12 +13,15 @@ defined('MOODLE_INTERNAL') || die();
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
 /**
+defined('MOODLE_INTERNAL') || die();
+
  * Upgrade script for mod_pptbook.
  *
  * @package   mod_pptbook
  * @category  upgrade
- * @copyright 2025 Ralf Hagemeister 
+ * @copyright 2025 Ralf Hagemeister
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 /**
